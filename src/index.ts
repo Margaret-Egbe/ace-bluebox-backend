8a3b408fb30f3cac0a4e44861adf122758ca6dbf
Margaret Egbe <marymargaret168@gmail.com>
2024-06-20 17:31:20 +0100

    added user restaurant features

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import MyRestaurantRoute from "./routes/MyRestaurantRoute";
+
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("connected to database!"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -15,6 +25,7 @@ app.get("/health", async (req: Request, res: Response) =>{
 });
 
  app.use("/api/my/user", myUserRoute);
+ app.use("/api/my/restaurant", MyRestaurantRoute)
 
 app.listen(5000, () => {
     console.log("server started on port 5000... ")
